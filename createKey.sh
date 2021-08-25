428c35b9fa41c5553dbffe1f36fcc7d455038740
rxzcode <wisehotline@gmail.com>
2021-08-25 12:29:14 +0700

    20210825:122914

--- createKey.sh
+++ createKey.sh
@@ -2,6 +2,6 @@
 openssl rand 16 > enc.key
 
 # FILE -> tier01.keyinfo
-echo "https://videomini.com/api/key/enc.key" > tier01.keyinfo
+echo "http://139.59.114.247:3000/api/key/enc.key" > tier01.keyinfo
 echo "enc.key" >> tier01.keyinfo
 echo `openssl rand -hex 16` >> tier01.keyinfo
