78ae5fd951dfe161ffbbaf8c40688cc47f0a6fdb
rxzcode <wisehotline@gmail.com>
2021-08-25 12:35:01 +0700

    20210825:123501

--- renderHls.sh
+++ renderHls.sh
@@ -1,4 +1,5 @@
 # bash renderHls.sh ./input/480p.mp4 ./input/720p.mp4 ./input/1080p.mp4 ./output/beach ./tier01.keyinfo
+# bash renderHls.sh ./input/480p.mp4 ./input/720p.mp4 ./input/1080p.mp4 ./output/beach ./tier01.keyinfo
 
 # 480p
 profile1="${1}"
@@ -47,9 +48,9 @@ fi
 touch "$destination/playlist.m3u8"
 echo "#EXTM3U" > "$destination/playlist.m3u8"
 echo "#EXT-X-VERSION:3" >> "$destination/playlist.m3u8"
-echo "#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480" > "$destination/playlist.m3u8"
+echo "#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480" >> "$destination/playlist.m3u8"
 echo "480p.m3u8" >> "$destination/playlist.m3u8"
-echo "#EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720" > "$destination/playlist.m3u8"
+echo "#EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720" >> "$destination/playlist.m3u8"
 echo "720p.m3u8" >> "$destination/playlist.m3u8"
-echo "#EXT-X-STREAM-INF:BANDWIDTH=5000000,RESOLUTION=1920x1080" > "$destination/playlist.m3u8"
+echo "#EXT-X-STREAM-INF:BANDWIDTH=5000000,RESOLUTION=1920x1080" >> "$destination/playlist.m3u8"
 echo "1080p.m3u8" >> "$destination/playlist.m3u8"
